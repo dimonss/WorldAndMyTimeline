0aad150ea8c28c87af14cf2942a732b976d4ad7f
Dmitrii <faimos69@gmail.com>
2026-03-27 20:35:10 +0600

    Update name and base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kyrgyzstan_and_my_way/',
+  base: '/world_and_my_way/',
   plugins: [react()],
 })
